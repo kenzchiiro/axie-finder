5f321aa6b00c0aab8ae53f322effe7660ee1a31e
kench1r0 <nattawut.eleven@gmail.com>
2021-09-05 13:14:57 +0700

    init code

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"axie-notify/services"
+	"axie-notify/services/delivery/http"
+	"log"
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+// func main() {
+
+// 	accessToken := "vJ5VjJJ6IM15AxZqFPMHelDJ9AwGrYvxJTie98xSWKJ"
+// 	message := "Found Axie#15326 on sale\nprice 0.12"
+// 	imageURL := "https://storage.googleapis.com/assets.axieinfinity.com/axies/15326/axie/axie-full-transparent.png"
+// 	if err := notify.SendImage(accessToken, message, imageURL); err != nil {
+// 		panic(err)
+// 	}
+// 	// accessToken := "vJ5VjJJ6IM15AxZqFPMHelDJ9AwGrYvxJTie98xSWKJ"
+// 	// message := "hello, world!"
+// 	// imageURL := "image url. ex) https://..."
+
+// 	// if err := notify.SendImage(accessToken, message); err != nil {
+// 	// 	panic(err)
+// 	// }
+
+// 	app := fiber.New()
+
+// 	app.Get("/posts", func(c *fiber.Ctx) error {
+// 		var result []Post
+// 		for _, post := range posts {
+// 			result = append(result, post)
+// 		}
+// 		return c.JSON(result)
+// 	})
+
+// 	app.Listen(":3000")
+
+// }
+
+func main() {
+	startService()
+}
+
+func connectLineBot() *linebot.Client {
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bot
+}
+
+func startService() {
+	e := echo.New()
+	bankCoreInfo := services.NewBankCoreServiceInfo()
+	http.NewServiceHTTPHandler(e, connectLineBot(), bankCoreInfo)
+	e.Logger.Fatal(e.Start(":6000"))
+}
---
+++ services/axie.go
@@ -0,0 +1,124 @@
+package services
+
+import (
+	"axie-notify/models"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func SetAxieParameter() (data *models.Payload) {
+	data = &models.Payload{
+		OperationName: `GetAxieBriefList`,
+		Query: `query GetAxieBriefList(
+			$auctionType: AuctionType,
+			$criteria: AxieSearchCriteria,
+			$from: Int,
+			$sort: SortBy,
+			$size: Int,
+			$owner: String)
+			{axies(
+				auctionType: $auctionType,
+				criteria: $criteria,
+				from: $from,
+				sort: $sort,
+				size: $size,
+				owner: $owner)
+				{total
+				results {
+					...AxieBrief
+					__typename  }
+				  __typename
+				  }      }
+				    fragment AxieBrief on Axie {
+						id
+						name
+						stage
+						class
+						breedCount
+						image
+						title
+						genes
+						battleInfo {  banned  __typename}
+						auction {
+							currentPrice
+							currentPriceUSD
+							__typename
+						}stats {
+							...AxieStats  __typename
+						}parts {
+							id
+							name
+							class
+							type
+							specialGenes
+							__typename
+							}
+							__typename
+						}
+						fragment AxieStats on AxieStats
+						{
+							hp
+							speed
+							skill
+							morale
+							__typename
+						}`,
+		Variables: models.Variables{
+			AuctionType: "Sale",
+			Criteria: models.Criteria{
+				Classes: []string{"Plant"},
+				Parts: []string{
+					"mouth-serious",
+					"mouth-humorless",
+					"horn-little-branch",
+					"horn-winter-branch",
+					"back-pumpkin",
+					"tail-carrot",
+					"tail-namek-carrot",
+				},
+				Hp:         nil,
+				Speed:      nil,
+				Skill:      nil,
+				Morale:     nil,
+				BreedCount: nil,
+				Pureness:   []int{5, 6},
+				NumMystic:  nil,
+				Title:      nil,
+				Region:     nil,
+				Stages:     []int{3, 4},
+			},
+			From:  0,
+			Size:  12,
+			Sort:  "PriceAsc",
+			Owner: nil,
+		},
+	}
+	return data
+}
+
+func GetAxie(data *models.Payload) {
+	data = SetAxieParameter()
+	payloadBytes, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	body := bytes.NewReader(payloadBytes)
+
+	req, err := http.NewRequest("POST", "https://axieinfinity.com/graphql-server-v2/graphql", body)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	res, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(res))
+}
