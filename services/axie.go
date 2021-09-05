17ac41c1dd9667e29992c9360dddfd03df91f47f
kench1r0 <nattawut.eleven@gmail.com>
2021-09-06 00:14:58 +0700

    update code

--- services/axie.go
+++ services/axie.go
@@ -296,6 +296,11 @@ func NewAxieFlexMessageTemplate() (flexMessage *linebot.FlexMessage) {
 
 	// Make Title
 	var titleContents []linebot.FlexComponent
+	titleIcon := linebot.IconComponent{
+		Type: linebot.FlexComponentTypeIcon,
+		URL:  "https://firebasestorage.googleapis.com/v0/b/filestore-1d8e6.appspot.com/o/class_beast.svg?alt=media&token=799d6dd8-a8a2-4d9e-8748-a4c0134e0a8f",
+	}
+	titleContents = append(titleContents, &titleIcon)
 	var span []*linebot.SpanComponent
 	titleText := linebot.TextComponent{
 		Type:     linebot.FlexComponentTypeText,
@@ -305,11 +310,7 @@ func NewAxieFlexMessageTemplate() (flexMessage *linebot.FlexMessage) {
 		Contents: span,
 	}
 	titleContents = append(titleContents, &titleText)
-	titleIcon := linebot.IconComponent{
-		Type: linebot.FlexComponentTypeIcon,
-		URL:  "https://firebasestorage.googleapis.com/v0/b/filestore-1d8e6.appspot.com/o/class_beast.svg?alt=media&token=799d6dd8-a8a2-4d9e-8748-a4c0134e0a8f",
-	}
-	titleContents = append(titleContents, &titleIcon)
+
 	titleBox := linebot.BoxComponent{
 		Type:     linebot.FlexComponentTypeBox,
 		Layout:   linebot.FlexBoxLayoutTypeBaseline,
